be43aaf53b11104276f50244aab072bd78b7d55a
FleurTamboezer <ftamboezer@gmail.com>
2018-02-08 15:29:50 +0100

    First sketches on my portfolio website

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'middleman', '~> 4.2'
+gem 'middleman-autoprefixer', '~> 2.7'
+gem 'middleman-livereload'
+
+
+group :development, :test do
+  gem 'capybara'
+  gem 'pry'
+  gem 'pry-byebug'
+  gem 'rspec'
+end
---
+++ config.rb
@@ -0,0 +1,17 @@
+activate :livereload
+
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+
+
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+# Layouts
+# https://middlemanapp.com/basics/layouts/
+
+# Per-page layout changes
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
