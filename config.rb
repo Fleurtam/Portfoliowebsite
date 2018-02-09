7cfac02d5426f780daadd6e3e2943e4814780cf2
FleurTamboezer <ftamboezer@gmail.com>
2018-02-09 10:15:02 +0100

    Deploy to githubpages

--- config.rb
+++ config.rb
@@ -2,24 +2,20 @@ activate :livereload
 
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
-
+require 'extensions/build_cleaner'
 
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-
-
 activate :deploy do |deploy|
-    require 'extensions/build_cleaner'
-
-    configure :build do
-      activate :relative_assets
-      activate :build_cleaner
-
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+configure :build do
+  activate :relative_assets
+  activate :build_cleaner
 end
 
 
