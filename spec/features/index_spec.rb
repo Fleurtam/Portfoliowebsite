c1dd6e3e5fc45673ebb6851407d45eefbb6efca5
FleurTamboezer <ftamboezer@gmail.com>
2018-02-09 13:50:22 +0100

    Started working on CV section

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -9,13 +9,25 @@ describe 'Index Page', type: :feature do
     end
   end
 
-    it 'renders footer partial' do
+  #  it 'renders footer partial' do
+  #  visit '/'
+
+  #  expect(page).to have_selector 'footer'
+  #  within 'footer' do
+  #    expect(page).to have_content 'My Portfolio'
+  #    expect(page).to have_content 'Built using the awesome Middleman framework'
+  #  end
+  #end
+
+  it 'displays cv' do
     visit '/'
 
-    expect(page).to have_selector 'footer'
-    within 'footer' do
-      expect(page).to have_content 'My Portfolio'
-      expect(page).to have_content 'Built using the awesome Middleman framework'
+    expect(page).to have_css '.cv'
+    within '.cv' do
+      expect(page).to have_content 'Curriculum Vitae'
+      expect(page).to have_content 'Professional experience'
+      expect(page).to have_content 'Education'
+      expect(page).to have_content 'Courses'
     end
   end
 end
