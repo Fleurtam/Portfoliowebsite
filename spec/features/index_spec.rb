640466c4c115c47e2bf7ac5d606a1b66211bd40e
FleurTamboezer <ftamboezer@gmail.com>
2018-02-11 16:37:14 +0100

    experimenting with specs

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -6,11 +6,18 @@ describe 'Index Page', type: :feature do
     within '.projects' do
       expect(page).to have_content 'My First Website'
       expect(page).to have_content 'FizzBuzz'
+          end
+
+    expect(page).to have_css '.cv'
+    within '.cv' do
+      expect(page).to have_content 'HISTORY'
       expect(page).to have_content 'Writer'
       expect(page).to have_content 'Marketeer'
       expect(page).to have_content 'Education'
-      expect(page).to have_content 'Current project'
-    end
+      expect(page).to have_content 'Language Skills'
+      expect(page).to have_content 'New Language Skills'
+      expect(page).to have_content 'Current Project'
+        end
     end
   end
 
